7cbdcbea8a5cce4e1bb3e5bb74d1de0b995fb7f4
YektaY <yektay1@gmail.com>
2023-12-14 10:47:42 -0800

    Zhe's fix to two of the windows tests that are failing

--- src/badger/tests/test_cli_basic.py
+++ src/badger/tests/test_cli_basic.py
@@ -18,16 +18,19 @@ def test_cli_main():
     assert exitcode == 0
 
     # Check output lines
-    outlines = out.split('\n')
-    assert len(outlines) == 8
+    outlines = out.splitlines()
+    assert len(outlines) == 6
 
     # Check name
     assert outlines[0] == 'name: Badger the optimizer'
 
     # Check version
     version = metadata.version('badger-opt')
-    assert outlines[1] == f'version: {version}'
-
+    try:  # yaml encoding number-like string differently
+        _ = float(version)
+        assert outlines[1] == f"version: '{version}'"
+    except ValueError:
+        assert outlines[1] == f'version: {version}'
 
 def test_list_algo():
     command = ['badger', 'generator']
